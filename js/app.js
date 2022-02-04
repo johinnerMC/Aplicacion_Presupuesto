567b3b4c2c38c16088c8f1506a8b1046beba2d56
johinner <johinner5721@gmail.com>
2022-02-04 14:28:18 -0500

    creacion clase construtora

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const ingresos = [
+    new Ingreso('salario',20000),
+    new Ingreso('carro',1700)
+    ];
+
+const egresos = [
+    new Egreso('perdida', 500),
+    new Egreso('full presdida',2000)
+];
+
+
+    
