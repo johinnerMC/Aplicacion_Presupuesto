35398cbd72f7c976f268a0462099d817cbdecb26
johinner <johinner5721@gmail.com>
2022-02-04 14:47:13 -0500

    recuperacion de elementos html a javaScript

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
 const ingresos = [
     new Ingreso('salario',20000),
-    new Ingreso('carro',1700)
+    new Ingreso('carro',50000)
     ];
 
 const egresos = [
     new Egreso('perdida', 500),
     new Egreso('full presdida',2000)
 ];
+
+let cargarApp = () =>{
+    cargarCabecero();
+}
+
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for (let ingreso of ingresos){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+let totalEgresos = () => {
+    let totalEgreso = 0;
+    for (let egreso of egresos){
+        totalEgreso += egreso.valor
+    }
+    return totalEgreso
+}
+
+let cargarCabecero =  () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalIngresos() / totalIngresos();
+    document.getElementById('Presupuesto').innerText = presupuesto;
+    document.getElementById('porcentaje').innerText = porcentajeEgreso;
+    document.getElementById('ingresos').innerText = totalIngresos();
+    document.getElementById('egresos').innerText = totalEgresos();
+}
 
 
     
